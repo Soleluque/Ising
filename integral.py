55283aff29a87fde5e2aa8e8611ed4f7e59b7e6e
Sol <sol@sol-HP-Laptop-15-bs0xx>
2019-05-27 19:50:54 -0300

    Esto está, capaz ahbria que ver lo del tiempo de termalizacion si pinta

--- integral.py
+++ integral.py
@@ -12,25 +12,36 @@ import matplotlib.pyplot as plt
 
 
 x=[]
+y=[]
 
 
 suma=0
+sumav=[]
 
 with open("integral.txt", "r") as file:
     plots = csv.reader(file, delimiter = " ")
     for row in plots:
         x.append(float(row[0]))
         
-
+for i in range(99999):
+    y.append(i)
 
 for i in range(99999):
     suma+=(x[i*10]**2)
+    
+#for i in range(99999): #esto todavia no está bien
+#    sumav.append(x[i*10]**2)
+#    sumav[i]=sumav[i]/99999
+    
     
 suma=suma/99999
 
 print(suma)
 
+
+
 plt.hist(x,bins=240)
+
 
 #plt.axis([-1,1,0,18000])
 
@@ -40,5 +51,5 @@ plt.title("Histograma")
 
 plt.savefig("gaussiana05.png")
 
-
-#plt.show()
+#plt.plot(y,sumav)
+plt.show()
