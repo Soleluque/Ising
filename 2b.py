f38d44044cbf9bfd008ab5e38d31b956a394a2a8
Sol <sol@sol-HP-Laptop-15-bs0xx>
2019-05-30 13:20:17 -0300

    Para mati

--- 2b.py
+++ 2b.py
@@ -18,14 +18,25 @@ j=[]
 #es=[]
 #e=[]
 vmm=[]
+xsd=[]
+msd=[]
+ceros=[]
 
 vme=[]
+ys2=[]
 
-for i in range(500):
+vm2=[]
+
+chi=[]
+
+
+for i in range(51):
     xs.append([])
     ms.append([])
     #es.append([])
-   
+    xsd.append([])
+    msd.append([])
+    ys2.append([])
 
 with open("magnetizacionb.txt", "r") as file:
     plots = csv.reader(file, delimiter = " ")
@@ -39,32 +50,74 @@ with open("magnetizacionb.txt", "r") as file:
             ms[l-1].append(float(row[1])) #magnetizacion
             #es[l-1].append(float(row[2])) #energia
             
+for i in range(len(xs)):
+    for k in range(len(xs[i])):
+        if k%1024==0:
+            xsd[i].append(xs[i][k])
+            msd[i].append(ms[i][k])
 
-for i in range(500):
-    vmm.append(st.mean(ms[i]))
+for i in range(len(ms)):
+    vmm.append(st.mean(msd[i]))
     
-for i in range(500):
-    vmm[i]=vmm[i]
+for i in range(len(vmm)):
+    vmm[i]=vmm[i]/1024
+    
+    
+for i in range(len(msd)):
+    for k in range(len(msd[i])):
+        ys2[i].append(msd[i][k])
+        
+for i in range(len(msd)):
+    for k in range(len(msd[i])):
+        msd[i][k]=msd[i][k]**2
+        
 
-#for i in range(500):
+for i in range(len(ys2)):
+    
+    vm2.append(st.mean(ys2[i])/1024)
+
+for i in range(len(vmm)):
+    chi.append(vm2[i]-(vmm[i]**2))
+    
+for i in range(len(chi)):
+    chi[i]=chi[i]    
+
+
+#for i in range(501):
 #    vme.append(st.mean(es[i]))
     
 j=np.array(j)
+
+#plt.plot(xs[0],ms[0],"o--")
 ajustem=(1-(np.sinh(2*j))**(-4))**(1/8)
 
+for i in range(30):
+    ceros.append(0)
 
-plt.plot(j,vmm,"o",label="Datos")
+plt.plot(j,np.abs(vmm),"o",label="Datos")
 plt.plot(j,ajustem,"-r",label="Ajuste teorico")
+plt.plot(j[:30],ceros,"-r") #esto así no va
 
-#plt.xlabel('J*')
-#plt.ylabel('Magnetizacion')
-#plt.title("Magnetizacion en funcion de J/T")
+plt.xlabel('J*')
+plt.ylabel('Magnetizacion')
+plt.title("Magnetizacion en funcion de J/T")
 
-#plt.legend()
+plt.legend()
 
-#plt.savefig("magnetizacionb.png")
+plt.savefig("magnetizacionb.png")
 
-#plt.show()
+plt.show()
+
+
+plt.plot(j,chi,"o")
+
+plt.xlabel("J*")
+plt.ylabel("Suceptibilidad")
+plt.title("Suceptibilidad Media por Sitio")
+
+plt.savefig("suceptibilidadb.png")
+
+plt.show()
     
 #plt.plot(j,vme,"o")
 
